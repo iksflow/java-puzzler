c97d6e462dacdb56a4ad2cd8d7ef53fffe441743
성승익 <seunglk.seong>
2020-02-11 18:05:41 +0900

    Til puzzle06

---
+++ src/utils/ConvertUtil.java
@@ -0,0 +1,26 @@
+package utils;
+
+public class ConvertUtil {
+	public static String spliter(String s, int len, String delimiter) {
+		StringBuilder sb = new StringBuilder(s);
+		sb.reverse();
+		int count = sb.length() - len;
+		while(0 < count) {
+			sb.insert(count, delimiter);
+			count = count - len;
+		}
+		sb.reverse();
+		return sb.toString();
+	}
+	
+	public static <T extends Number> String binaryFormatter(T num) {
+		Long number = 0L;
+		if (num instanceof Integer) {
+			number = Long.valueOf((Integer) num); 
+		}
+		if (num instanceof Long) {
+			number = Long.valueOf((Long)num); 
+		}
+		return String.format("%32s", Long.toBinaryString(number)).replace(" ", "0");
+	}
+}
