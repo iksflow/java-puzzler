a567fd22a6aef1f7726f368e6d698b0de5726ca6
성승익 <seunglk.seong>
2020-02-13 18:05:44 +0900

    puzzle06 Sign extension

--- src/puzzle/Puzzle06.java
+++ src/puzzle/Puzzle06.java
@@ -3,7 +3,15 @@ package puzzle;
 public class Puzzle06 {
 
 	public static void main(String[] args) {
-
+		System.out.println((int)(char)(byte)-1);
+		byte b = -1;
+		char c = (char)b;
+		int i = (int)c;
+		// 0xFF : 비트마스크
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(Integer.toBinaryString(1024));
 	}
 
 }
