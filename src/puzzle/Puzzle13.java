346c5f30f138149ca539c98096a3afc7c16d2fe0
rotcafer <rotcafer@gmail.com>
2020-03-22 23:19:35 +0900

    13번째 퍼즐 동물농장

---
+++ src/puzzle/Puzzle13.java
@@ -0,0 +1,12 @@
+package puzzle;
+
+public class Puzzle13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final String pig = "length: 10";
+		final String dog = "length: " + pig.length();
+		System.out.println("Animals are equal: " + pig == dog);
+	}
+
+}
