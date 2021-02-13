ebbed1b6bb76a4ab6b68b2fcec9612b8279734c6
iksflow <theiksflow@gmail.com>
2021-02-13 21:05:30 +0900

    Puzzle14 학습

---
+++ src/puzzle/Puzzle14.java
@@ -0,0 +1,9 @@
+package puzzle;
+
+public class Puzzle14 {
+    public static void main(String[] args) {
+//        System.out.println("a\u0022.length() + \u0022b".length());
+        System.out.println("a\u0022.length());
+        System.out.println('\u0022');
+    }
+}
