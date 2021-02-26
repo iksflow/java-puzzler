7deb94c11e0a70c773e65d52f5d882d32e7836a2
iksflow <theiksflow@gmail.com>
2021-02-26 17:44:51 +0900

    Puzzle22 학습

---
+++ src/puzzle/Puzzle22.java
@@ -0,0 +1,9 @@
+package puzzle;
+
+public class Puzzle22 {
+    public static void main(String[] args) {
+        System.out.println("iexplore");
+        http://www.google.com;
+        System.out.println(":maximize");
+    }
+}
