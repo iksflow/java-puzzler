f0c03e91a10d8690e91a572be5b9bd5ff1769441
iksflow <theiksflow@gmail.com>
2021-03-02 23:37:12 +0900

    Puzzle26 학습

---
+++ src/puzzle/Puzzle26.java
@@ -0,0 +1,14 @@
+package puzzle;
+
+public class Puzzle26 {
+    public static final int END = Integer.MAX_VALUE;
+    public static final int START = END - 100;
+
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = START; i <= END; i++) {
+            count++;
+        }
+        System.out.println(count);
+    }
+}
