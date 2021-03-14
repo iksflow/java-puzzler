a8f3c97580652e2955d4218eb73d884b64011e54
iksflow <theiksflow@gmail.com>
2021-03-14 21:15:15 +0900

    Puzzle28 학습

---
+++ src/puzzle/Puzzle28.java
@@ -0,0 +1,10 @@
+package puzzle;
+
+public class Puzzle28 {
+    public static void main(String[] args) {
+        double i = Double.POSITIVE_INFINITY;
+        while (i == i + 1) {
+            System.out.println("inf");
+        }
+    }
+}
