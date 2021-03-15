946ef2ba4dda0f8a1229bda240a967460cffa4ec
iksflow <theiksflow@gmail.com>
2021-03-15 21:03:45 +0900

    Puzzle29 Java의 Nan을 알게됨

---
+++ src/puzzle/Puzzle29.java
@@ -0,0 +1,11 @@
+package puzzle;
+
+public class Puzzle29 {
+    public static void main(String[] args) {
+        double i = 0.0 / 0.0;
+        System.out.println(i);
+        while (i != i) {
+            System.out.println("inf");
+        }
+    }
+}
