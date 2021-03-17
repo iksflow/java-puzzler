6d756af8c0587db8c6521189aa66c0aba614a3b1
iksflow <theiksflow@gmail.com>
2021-03-17 20:00:00 +0900

    Puzzle30 학습

---
+++ src/puzzle/Puzzle30.java
@@ -0,0 +1,10 @@
+package puzzle;
+
+public class Puzzle30 {
+    public static void main(String[] args) {
+        String i = "abc";
+        while (i != i + 0) {
+            System.out.println("inf");
+        }
+    }
+}
---
+++ src/puzzle/Puzzle31.java
@@ -0,0 +1,11 @@
+package puzzle;
+
+public class Puzzle31 {
+    public static void main(String[] args) {
+        byte i = 1;
+        while (i != 0) {
+            i >>>= 1;
+            System.out.println("inf");
+        }
+    }
+}
