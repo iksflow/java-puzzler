3dc45f2b52553894b469ff83ca2ae9ec218b3c8b
iksflow <theiksflow@gmail.com>
2021-03-25 17:42:49 +0900

    Puzzle36 finally 문에서 return 사용하면 생기는일

---
+++ src/puzzle/Puzzle36.java
@@ -0,0 +1,15 @@
+package puzzle;
+
+public class Puzzle36 {
+    public static void main(String[] args) {
+        System.out.println(decision());
+    }
+
+    static boolean decision() {
+        try {
+            return true;
+        } finally {
+            return false;
+        }
+    }
+}
