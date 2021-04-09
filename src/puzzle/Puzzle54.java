a244e8d4a0e1ddc3fbbb9907bd2805e2cbdb854a
iksflow <theiksflow@gmail.com>
2021-04-09 23:43:42 +0900

    Puzzle54 null의 정적메서드 호출

---
+++ src/puzzle/Puzzle54.java
@@ -0,0 +1,11 @@
+package puzzle;
+
+public class Puzzle54 {
+    public static void greet() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        ((Puzzle54)null).greet();
+    }
+}
